724570eacd3490487cde21cc065e8315e4dbe352
Mauro Busso <mauro.busso12@gmail.com>
2023-09-20 17:21:43 +0100

    made icond on nav bar

--- src/Components/navigation/Navbar.jsx
+++ src/Components/navigation/Navbar.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import { Logo } from '../../Assets/Assets'
-import {  HiOutlineUser, HiOutlineShoppingCart } from "react-icons/hi"
+import {  HiOutlineSearch, HiOutlineUser, HiOutlineShoppingCart, HiOutlineHeart } from "react-icons/hi"
 import { NavLink } from 'react-router-dom'
 
 //this component is not responsive for mobile
 
 const Navbar = () => {
+  
+
+
+  const icons = [
+    {
+      path: "/",
+      icon: < HiOutlineSearch/>
+    },
+    {
+      path: "/",
+      icon: < HiOutlineUser/>
+    },
+    {
+      path: "/",
+      icon: < HiOutlineHeart/>
+    },
+    {
+      path: "/",
+      icon: < HiOutlineShoppingCart/>
+    }
+  ]
+
   return (
     <header className=' box-border h-24 w-full'>
       <section className=' bg-primary w-full h-10'></section>
@@ -51,12 +73,19 @@ const Navbar = () => {
           </div>
           <div>
             <section className='flex gap-6 font-semibold'>
+
+              {
+                icons.map((item, index) => (
+
+                  <NavLink
+                      to={item.path}
+                      key={index}                  
+                  >
+                    <div className='text-lg font-medium p-4 hover:bg-slate-400'>{item.icon}</div>
+                  </NavLink>
+                ))
+              }
               
-              <NavLink
-                  to='/'                  
-              >
-                <div className='text-lg font-medium p-4 hover:bg-slate-400'>< HiOutlineUser/></div>
-              </NavLink>
 
               {/* <li className='hover:bg-slate-400 p-4'><button>icon</button></li>
               <li className='hover:bg-slate-400 p-4'><button>icon</button></li>
